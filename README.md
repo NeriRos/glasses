6e9cec4a726efbdd2567a6c7fbfa1d4d6e4c388c
neriyarosner <neriyarosner@gmail.com>
2018-02-20 13:55:55 +0200

    added bower installation to init, pretified readme.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # ex1
 targil from harel
 
-# Prerequisites
+## Prerequisites
 1. install mongodb.
 2. install nodejs.
 3. install npm.
 
-# Installation process
+## Installation process
 1. clone the project where ever you like.
 2. cd into the project.
 3. execute init.bat.
--- init.bat
+++ init.bat
@@ -1,17 +1,8 @@
 @echo off
 
-echo [+] installing back end prerequisites.
-cmd.exe /C "cd  %~dp0%backEnd && npm install"
-echo.
-echo [+] installing back end modules prerequisites.
-cmd.exe /C "cd  %~dp0%backEnd\modules && npm install"
 echo. 
-echo [+] installing front end prerequisites.
-cmd.exe /C "cd  %~dp0%frontEnd && npm install"
-echo.
-echo [+] installing front end assets prerequisites.
-cmd.exe /C "cd  %~dp0%frontEnd\src\assets && bower install"
-
+echo [+] installing bower
+npm install -g bower
 echo. 
 echo [+] installing nodemon
 npm install -g nodemon
@@ -19,7 +10,16 @@ echo.
 echo [+] installing angular cli
 npm install -g @angular/cli
 
+echo [+] installing back end prerequisites.
+cmd.exe /C "cd  %~dp0%backEnd && npm install"
 echo. 
-echo. 
+echo [+] installing front end prerequisites.
+cmd.exe /C "cd  %~dp0%frontEnd && npm install"
+echo.
+echo [+] installing front end assets prerequisites.
+cmd.exe /C "cd  %~dp0%frontEnd\src\assets && bower install"
+
+echo.
+echo.
 echo [*] DONE! YOU CAN CLOSE NOW.
 pause
